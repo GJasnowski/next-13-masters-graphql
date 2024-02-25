bb49f401ff69b985691b2a73a74f542ac47023c9
GJasnowski <gustaw.jasnowski@gmail.com>
2024-02-25 18:19:37 +0100

    refine order status values

---
+++ prisma/migrations/20240225171910_refine_order_status_values/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [SUCCESS,FAILURE] on the enum `order_status` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "order_status_new" AS ENUM ('PENDING', 'COMPLETED', 'CANCELLED');
+ALTER TABLE "order" ALTER COLUMN "status" TYPE "order_status_new" USING ("status"::text::"order_status_new");
+ALTER TYPE "order_status" RENAME TO "order_status_old";
+ALTER TYPE "order_status_new" RENAME TO "order_status";
+DROP TYPE "order_status_old";
+COMMIT;
